bc318af362936d3c0882cc6f845c0291dbbecc87
Dmitry Sergeev <dmitrijsergeev@gmail.com>
2024-01-28 14:03:42 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+function correct(s){
+    s = s.split('');
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '5') s[i] = 'S';
+        else if (s[i] === '0') s[i] = 'O';
+        else if (s[i] === '1') s[i] = 'I';
+    }
+    return s.join('');
+}
+
+console.log(correct('J0hn'));
+
+let arr = 'George';
+let ddd = arr.split('');
+console.log(ddd);
+console.log(ddd.join(''));
